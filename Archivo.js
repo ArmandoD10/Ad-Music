a7b6a7f2b41b89e577bd87cbeaf33aad88c22b62
ArmandoD10 <armandodiaz1020158@gmail.com>
2025-11-24 23:20:46 -0400

    Add files via upload

--- Archivo.js
+++ Archivo.js
@@ -4,3 +4,35 @@ document.documentElement.classList.add("dark");
 document.getElementById("btnTema").addEventListener("click", function () {
     document.documentElement.classList.toggle("dark");
 });
+
+document.getElementById("btnLogin").addEventListener("click", login);
+
+async function login() {
+    let correo = document.getElementById("usuario").value.trim();
+    let password = document.getElementById("contrasena").value.trim();
+
+    if (correo === "" || password === "") {
+        alert("Debe llenar todos los campos.");
+        return;
+    }
+
+    const { data, error } = await supabase
+        .from("usuario")
+        .select("*")
+        .eq("Correo", correo)
+        .single();
+
+    if (error || !data) {
+        alert("Correo no encontrado.");
+        return;
+    }
+
+    if (data.password !== password) {
+        alert("Contraseña incorrecta.");
+        return;
+    }
+
+    // Login exitoso
+    alert("Bienvenido " + data.correo);
+    window.location.href = "Menu.html"; // crea esta página
+}
